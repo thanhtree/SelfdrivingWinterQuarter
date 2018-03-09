49e9c6d22dd12e2b20c5b3dbe713f96536806f36
Kalani Murakami <kalanimurakami1218@gmail.com>
2018-03-08 22:10:57 -0800

    Uploading Simulations projects

---
+++ Simulations/VPilot-master/createmodel.py
@@ -0,0 +1,3 @@
+import h5py
+import numpy as np
+f = h5py.File("model.h5", "w")
---
+++ Simulations/VPilot-master/train.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+from keras.optimizers import RMSprop
+from keras.callbacks import ModelCheckpoint
+
+from model import nanoAitorNet
+
+def acceptSample(sample):
+	throttle = sample[1][4]
+	steering = sample[1][3]
+
+	if (np.absolute(steering) > 0.025 and throttle > 0.15):
+		return True
+
+	if(throttle < 0.15 or np.absolute(steering) < 0.025):
+		if(np.random.rand() > 0.95):
+			return True
+
+	return False
+
+def plotHist(dataset, n):
+	values = [sample[1][n]for sample in dataset]
+
+	plt.hist(values, bins='auto')
+	plt.show()
+
+if __name__ == '__main__':
+
+	datasetFiles = ['G:\GTAVDataset\dataset.txt']
+
+	aitorNet = nanoAitorNet()
+
+	dataset = aitorNet.toSequenceDataset(datasetFiles)
+	dataset = [sample for sample in dataset if acceptSample(sample)]
+
+	valLen = int(len(dataset)*0.2)
+	valDataset = dataset[0:valLen]
+	dataset = np.delete(dataset, np.s_[0:valLen], 0)
+
+	trainGenerator = aitorNet.dataGenerator(dataset)
+	valGenerator = aitorNet.dataGenerator(valDataset)
+
+	#model = aitorNet.getModel(weights_path="model.h5")
+	model = aitorNet.getModel(weights_path="model.hdf5")
+	model.compile(optimizer=RMSprop(), loss='mse')
+	#ckp_callback = ModelCheckpoint("model.h5", monitor="val_loss", save_best_only=True, save_weights_only=True, mode='min')
+	ckp_callback = ModelCheckpoint("model.hdf5", monitor="val_loss", save_best_only=True, save_weights_only=True, mode='min')
+
+	model.fit_generator(
+		trainGenerator,
+		samples_per_epoch=len(dataset),
+		nb_epoch=1000,
+		validation_data=valGenerator,
+		nb_val_samples=len(valDataset),
+		callbacks=[ckp_callback]
+	)
